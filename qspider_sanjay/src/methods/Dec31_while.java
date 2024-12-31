89f95d68b9522bef80f52feec3bee87a03ca1004
sanjayv23 <sanjayvelu2311@gmail.com>
2024-12-31 10:42:35 +0530

    create first commit

--- qspider_sanjay/src/methods/Dec31_while.java
+++ qspider_sanjay/src/methods/Dec31_while.java
@@ -6,5 +6,36 @@ public class Dec31_while {
 		while (ch<='Z') {
 			System.out.print(ch+++" ");
 		}
+		System.out.println();
+		
+		//reverse the number
+		System.out.println();
+		int num=156;
+		int org=num;
+		int rev=0;
+		while(num!=0) {
+			int rem=num%10;
+			rev=rev*10+rem;
+			num/=10;
+			
+		}
+		System.out.println("Reverse of number: "+rev);	
+		
+		//palindrome
+		System.out.println();
+		if(org==rev)System.out.println("palindrome");
+		else System.out.println("not plaindrome");
+		
+		//Sum of digit
+		System.out.println();
+		int num1=5864;
+		int sum=0;
+		while(num1!=0) {
+			int rev1=num1%10;
+			sum+=rev1;
+			num1/=10;
+			
+		}
+		System.out.println("sum of digit: "+sum);
 	}
 }
--- qspider_sanjay/src/methods/Pratice_area.java
+++ qspider_sanjay/src/methods/Pratice_area.java
@@ -1,29 +1,17 @@
 package methods;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Pratice_area {
 	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		//String str=scan.next();
-        //System.out.println(str.equals("male")); //see notes
-        //sum(2,3,2);
+		
 		boolean condition = false;
 		while(condition) {
 		    // Logic here
 		}
-
+		//scan.close();
         
 	}
-	public static void sum(int a,int b) {
-		System.out.println(2);
-		return a+b;
-	}
-	public static void sum(int a,int b) {
-		System.out.println(3);
-	}
-	public static void sum(int a,int b,int c,int d) {
-		System.out.println(4);
-	}
+	
 	
 }

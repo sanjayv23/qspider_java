8677359c577717dd511dcf47a6ae32bdd10ecb51
sanjayv23 <sanjayvelu2311@gmail.com>
2025-05-15 21:29:27 +0530

    edit: may15 files

--- qspider_sanjay/src/may15/BagDriver.java
+++ qspider_sanjay/src/may15/BagDriver.java
@@ -7,15 +7,12 @@ public class BagDriver {
 		b1.setPrice(2500);
 		System.out.println(b1);
 		System.out.println("Color: "+b1.getColor());
-		System.out.println("price: "+b1.getPrice());
+		System.out.println("PRICE: "+b1.getPrice());
 		Bag b2=new Bag();
 		System.out.println(b2);
 		
 		
-		b2.setColor("White");
-		b2.setPrice(5000);
-		System.out.println("Color: "+b2.getColor());
-		System.out.println("price: "+b2.getPrice());
+		
 		
 	}
 }

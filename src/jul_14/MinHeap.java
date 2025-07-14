dbb43c08c8496bd670a3f95762ebad6735af0f80
sanjayv23 <sanjayvelu2311@gmail.com>
2025-07-14 14:02:48 +0530

    edit:  MinHeap

--- src/jul_14/MinHeap.java
+++ src/jul_14/MinHeap.java
@@ -10,11 +10,11 @@ public class MinHeap {
 		arr=new int[size+1];
 	}
 	public void insert(int val) {
-		if(size>=arr.length-1) return;
+		if(IsFull()) return;
 		arr[++size]=val;
-		heapfy(size);
+		HeapfyBottomTop(size);
 	}
-	private void heapfy(int ind) {
+	public void HeapfyBottomTop(int ind) {
 		if(ind<=1) return;
 		int parent=ind/2;
 		if(arr[parent]>arr[ind]) {
@@ -22,14 +22,52 @@ public class MinHeap {
 			arr[parent]=arr[ind];
 			arr[ind]=temp;
 		}
-		heapfy(parent);
+		HeapfyBottomTop(parent);
 	}
 	
 	public void display() {
-		
 		System.out.println(Arrays.toString(arr));
 	}
 	
+	public Integer peek() {
+		return IsEmpty()?null:arr[1];
+	}
+	
+	public boolean IsEmpty() {
+		
+		return size==0?true:false;
+	}
+	public boolean IsFull() {
+		
+		return size==arr.length-1?true:false;
+	}
+	
+	public void delete() {
+		if(IsEmpty()) {
+			System.out.println("Heap Empty");
+			return;
+			
+		}
+		arr[1]=arr[size--];
+		HeapifyTopBottom(1);
+	}
+	private void HeapifyTopBottom(int ind) {
+		int left=ind*2;
+		int right=ind*2+1;
+		if(size<left) return;
+		if(size==left && arr[left]<arr[ind]) {
+			// swap arr[left] and a[ind]			
+			arr[left]=(arr[left]+arr[ind])-(arr[ind]=arr[left]);
+		}
+		else {
+			int min=arr[left]<arr[right]?left:right;
+			if(arr[ind]>arr[min]) {
+				// swap arr[ind] and arr[min]
+				arr[ind]=(arr[ind]+arr[min])-(arr[min]=arr[ind]);
+				HeapifyTopBottom(min);
+			}
+		}
+	}
 	public static void main(String[] args) {
 		int[] num= {15,6,42,16,21,45,1};
 		MinHeap obj=new MinHeap(num.length);
@@ -38,7 +76,7 @@ public class MinHeap {
 		for(int a:num) {
 			obj.insert(a);
 		}
-		
+		obj.delete();
 		obj.display();
 		
 		
